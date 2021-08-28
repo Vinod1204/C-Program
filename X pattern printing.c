e534a76e45bb3c8ab84718e2259dcb58523d1996
VINOD M <89124517+Vinod1204@users.noreply.github.com>
2021-08-29 01:58:22 +0530

    Add files via upload

---
+++ X pattern printing.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main() {
+
+    int n,i,j,m;
+    scanf("%d",&n);
+    m=(n+1)/2;
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {  
+            if(i==j)
+            {
+            if(i<m)
+            {
+                printf("%d ",(n-i+1));
+            }
+            else 
+            {
+                printf("%d",i);
+            }
+            }
+            else if(i+j==n+1)
+            {
+            if(i<m)
+            {
+                printf("%d ",i);
+            }
+            else
+            {
+                printf("%d ",j);
+            }
+            }
+            else
+            {
+            printf("  ");
+            }
+        }
+    printf("\n");
+    }
+    return 0;
+}
+
